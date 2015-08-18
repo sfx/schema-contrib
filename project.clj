3e0c51e6e51fbd758344ecb5c221e338eefcc3a2
Antony Woods <acron1@gmail.com>
2015-08-18 12:47:24 +0100

    Updated dependencies, including Clojure to 1.7

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject schema-contrib "0.1.5"
+(defproject schema-contrib "0.1.6-SNAPSHOT"
   :description "Additional validators for Prismatic's Schema."
   :url "https://github.com/sfx/schema-contrib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[instaparse "1.3.0"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.7"]
-                 [prismatic/schema "0.2.6"]]
+  :dependencies [[instaparse "1.4.1"]
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/test.check "0.8.0"]
+                 [prismatic/schema "0.4.3"]]
   :resource-paths ["resources"])
