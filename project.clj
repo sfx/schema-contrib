abdac6f596d8ce9477be621943a3201a4105dcb9
Turkka Mannila <turkka.mannila@vincit.fi>
2018-01-20 17:32:57 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/sfx/schema-contrib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[instaparse "1.3.0"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.7"]
-                 [prismatic/schema "0.2.6"]]
+  :dependencies [[instaparse "1.4.8"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/test.check "0.9.0"]
+                 [prismatic/schema "1.1.7"]]
   :resource-paths ["resources"])
