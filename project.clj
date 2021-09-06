913a84b471c4869103d10999aefca820ff51dffd
Sindhu S <sindhus@live.in>
2021-09-06 20:44:35 +0530

    new version 0.2.0 and new dependencies for all libraries

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject schema-contrib "0.1.5"
+(defproject io.bytebeam/schema-contrib "0.2.0"
   :description "Additional validators for Prismatic's Schema."
   :url "https://github.com/sfx/schema-contrib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[instaparse "1.3.0"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.7"]
-                 [prismatic/schema "0.2.6"]]
+  :dependencies [[instaparse "1.4.10"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/test.check "1.1.0"]
+                 [prismatic/schema "1.1.12"]]
   :resource-paths ["resources"])
